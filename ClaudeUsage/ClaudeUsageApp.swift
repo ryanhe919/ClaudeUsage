9e29f001843de1e66e10661c6463bdd5b2935755
Yufan He <ryanhe0919@gmail.com>
2026-02-20 16:51:32 +0800

    Initial Commit

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Yufan He on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
